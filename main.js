81d8b2ced83b9984fa6b305bd1cd16011c3321e4
AshwagAlharthi <Ashwag.alharthi@hotmail.com>
2024-09-15 13:24:08 +0300

    solved

---
+++ main.js
@@ -0,0 +1,22 @@
+let url = 'https://coffee.alexflipnote.dev/random.json';
+let container = document.getElementById("container");
+
+function changingImage() {
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+       
+        let img = document.createElement("img");
+
+        container.innerHTML = '';
+        img.src = data.file;
+        img.classList.add("image");
+       
+        container.appendChild(img);
+
+    })
+}
+
+changingImage();
+setInterval(changingImage, 1000); 
